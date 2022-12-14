2ccdf79c6e9d58db281e765b2571afe71fa984e9
Miles Kerr <m.kerr.professional@gmail.com>
2022-12-14 08:48:21 -0600

    commit tree e6de074aa6e263dbe2a3ee0ead7e4a03c3ddf467

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+cats.slice("Milo", "Otis", "Garfield");
+function destructivelyAppendCat(name) {
+    cats.splice(4, 0, "Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.splice(0, 0, "Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.splice(2, 1);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.splice(0,1);
+}
+function appendCat(name) {
+   const newCats=['Broom', ...cats];
+}
